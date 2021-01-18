1be28c16618e5e5ddf70429f5dcb5306017df67b
Leonard Ugbanawaji Ekenekiso <senenerst@gmail.com>
2021-01-18 15:54:50 +0000

    initialized laravel app and generate user, role and permission model

---
+++ app/Http/Controllers/RolesController.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Role;
+use Illuminate\Http\Request;
+
+class RolesController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $role = Role::orderBy('id', 'desc')->get();
+        return view('admin.role.index', compact('role'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('admin.roles.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $role = new Role;
+        $role->name = $request->name;
+        $role->slug = $request->slug;
+        $role->save();
+        $listOFPermission = explode(',', $request->roles_permissions); //create array from comma seperated string
+        foreach($listOFPermission as $permission){
+            $permissions = new Permission;
+            $permissions->name = $permission;
+            $permissions->slug = strtolower(str_replace(" ", "-", $permission));
+            $permissions->save();
+            $role->permissions()->attach($permissions->id);
+            $role->save();
+        }
+        return redirect('/roles')->with('success', 'role created successfully.');
+
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Role  $role
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Role $role)
+    {
+        return view('admin.roles.show', compact('role'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Role  $role
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Role $role)
+    {
+        return view('admin.roles.edit', compact('role'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Role  $role
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Role $role)
+    {
+        $role->name = $request->name;
+        $role->slug = $request->slug;
+        $role->save();
+        return redirect('/roles')->with('success', 'role updated successfully');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Role  $role
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Role $role)
+    {
+        $role->delete();
+        return redirect('/roles')->with('success', 'role deleted successfully.');
+    }
+}
