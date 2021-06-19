ca3e52ee605e8619d82afd2bfd397c133fcf27d0
Leonard Ugbanawaji Ekenekiso <senenerst@gmail.com>
2021-06-19 06:15:27 +0100

    removed request logging

--- app/Providers/SchoolServiceProvider.php
+++ app/Providers/SchoolServiceProvider.php
@@ -31,8 +31,7 @@ class SchoolServiceProvider extends ServiceProvider
             $extract = new Extract();
             //$school = School::whereDomain($app['config']->get('app.url'))->first();
             $result = $extract->parse(request()->getHost());
-            dd($result->getFullHost(), $result->getRegistrableDomain());
-            $school = School::where('domain', str_replace('www.', '', $result->getFullHost()))->orWhere('alias', str_replace('www.', '', $result->getRegistrableDomain()))->first();
+            $school = School::where('domain', str_replace('www.', '', $result->getRegistrableDomain()))->orWhere('alias', str_replace('www.', '', $result->getFullHost()))->first();
             abort_unless($school, 404);
             return new Schools($school);
         });
